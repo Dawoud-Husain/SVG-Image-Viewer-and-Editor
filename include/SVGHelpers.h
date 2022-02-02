e22d6c17f1aae8c70b9f09bf68af0379722823e1
Dawoud Husain <husaind@fred.socs.uoguelph.ca>
2022-02-01 19:25:04 -0500

    changed helpers

---
+++ include/SVGHelpers.h
@@ -0,0 +1,50 @@
+#ifndef SVGADDITIONALHELPER_H
+#define SVGADDITIONALHELPER_H
+
+#include <stdio.h>
+#include <libxml/parser.h>
+#include <libxml/tree.h>
+#include <libxml/encoding.h>
+#include <libxml/xmlwriter.h>
+#include <libxml/xmlschemastypes.h>
+#include "LinkedListAPI.h"
+
+// Utility Functions
+void allocateFlexArray(char str[], char* src);
+
+// Module 1 Helper Functions
+void  getNameSpace(xmlNode * a_node, char namespace[256]);
+
+void getTitle(xmlNode * a_node, char title[256]);
+void getDescription(xmlNode * a_node, char description[256]);
+
+List* getRectsFromNode(xmlNode * a_node, List* rectsList);
+List* getCirclesFromNode(xmlNode * a_node, List * circlesList);
+List* getGroupsFromNode(xmlNode * a_node, List* groupsList);
+List* getPathsFromNode(xmlNode * a_node, List* pathsList);
+List* getOtherAttributesFromNode(xmlNode * a_node, List* attributeList);
+
+Rectangle * getRectFromSingleNode(xmlNode * a_node);
+Circle * getCircleFromSingleNode(xmlNode * a_node);
+Path * getPathFromSingleNode(xmlNode * a_node);
+Group * getGroupFromSingleNode(xmlNode * a_node);
+
+
+// Module 2 Helper Functions
+void getRectsFromAllGroups(List* rectsList, List*groupsList);
+void getCirclesFromAllGroups(List* circlesList, List*groupsList);
+void getPathsFromAllGroups(List* circlesList, List*groupsList);
+void getGroupsFromAllGroups(List* groupsList, List*groupsListInput);
+
+int compare(List * list, int (*customCompare)(const void* first,const void* second), const void* searchRecord);
+int compareRectanglesAreaFunc(const void* first,const void* second);
+int compareCirclesAreaFunc(const void* first,const void* second);
+
+
+int comparePathsMain(List * list, const char * searchRecord);
+int comparePathsDataFunc(char * first, const char * second);
+
+int compareGroupsLenFunc(const void* first,const void* second);
+
+
+#endif

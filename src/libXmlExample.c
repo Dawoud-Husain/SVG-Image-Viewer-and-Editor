17549a8436a616ce1084774b5fe06dfadeb10625
Dawoud Husain <husaind@fred.socs.uoguelph.ca>
2022-02-19 12:54:32 -0500

    inital A2 commit

--- src/libXmlExample.c
+++ src/libXmlExample.c
@@ -1,14 +1,16 @@
-/**
- * section: Tree
- * synopsis: Navigates a tree to print element names
- * purpose: Parse a file to a tree, use xmlDocGetRootElement() to
- *          get the root element, then walk the document and print
- *          all the element name in document order.
- * usage: tree1 filename_or_URL
- * test: tree1 test2.xml > tree1.tmp && diff tree1.tmp $(srcdir)/tree1.res
- * author: Dodji Seketeli
- * copy: see Copyright for the status of this software.
- */
+
+
+// **
+//  * section: Tree
+//  * synopsis: Navigates a tree to print element names
+//  * purpose: Parse a file to a tree, use xmlDocGetRootElement() to
+//  *          get the root element, then walk the document and print
+//  *          all the element name in document order.
+//  * usage: tree1 filename_or_URL
+//  * test: tree1 test2.xml > tree1.tmp && diff tree1.tmp $(srcdir)/tree1.res
+//  * author: Dodji Seketeli
+//  * copy: see Copyright for the status of this software.
+//  */
 #include <stdio.h>
 #include <libxml/parser.h>
 #include <libxml/tree.h>
@@ -37,9 +39,9 @@ print_element_names(xmlNode * a_node)
 
         // Uncomment the code below if you want to see the content of every node.
 
-        // if (cur_node->content != NULL ){
-        //     printf("  content: %s\n", cur_node->content);
-        // }
+        if (cur_node->content != NULL ){
+            printf("  content: %s\n", cur_node->content);
+        }
 
         // Iterate through every attribute of the current node
         xmlAttr *attr;
@@ -48,7 +50,7 @@ print_element_names(xmlNode * a_node)
             xmlNode *value = attr->children;
             char *attrName = (char *)attr->name;
             char *cont = (char *)(value->content);
-            printf("\tattribute name: %s, attribute value = %s\n", attrName, cont);
+            printf("\tattribute name: %s, attribute value = %s node name = %s\n", attrName, cont, cur_node->name);
         }
 
         print_element_names(cur_node->children);
@@ -103,3 +105,223 @@ main(int argc, char **argv)
 
     return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// /**
+//  * section: Tree
+//  * synopsis: Navigates a tree to print element names
+//  * purpose: Parse a file to a tree, use xmlDocGetRootElement() to
+//  *          get the root element, then walk the document and print
+//  *          all the element name in document order.
+//  * usage: tree1 filename_or_URL
+//  * test: tree1 test2.xml > tree1.tmp && diff tree1.tmp $(srcdir)/tree1.res
+//  * author: Dodji Seketeli
+//  * copy: see Copyright for the status of this software.
+//  */
+// #include <stdio.h>
+// #include <string.h>
+// #include <ctype.h>
+
+// #include <libxml/parser.h>
+// #include <libxml/tree.h>
+
+// /*
+//  *To compile this file using gcc you can type.  Alternatively, see the Makefile for include path settings
+//  *gcc `xml2-config --cflags --libs` -o xmlexample libxml2-example.c
+//  */
+
+// /**
+//  * print_element_names:
+//  * @a_node: the initial xml node to consider.
+//  *
+//  * Prints the names of the all the xml elements
+//  * that are siblings or children of a given xml node.
+//  */
+// // static void
+// // print_element_names(xmlNode * a_node)
+// // {
+// //     xmlNode *cur_node = NULL;
+
+// //     for (cur_node = a_node; cur_node != NULL; cur_node = cur_node->next) {
+// //         if (cur_node->type == XML_ELEMENT_NODE) {
+// //             printf("node type: Element, name: %s\n", cur_node->name);
+// //              printf("next %s\n", (char*)(cur_node->next)	);
+// //         }
+
+// //         // Uncomment the code below if you want to see the content of every node.
+
+// //         // if (cur_node->content != NULL ){
+// //         //     printf("  content: %s\n", cur_node->content);
+// //         // }
+
+// //         // Iterate through every attribute of the current node
+// //         xmlAttr *attr;
+// //         for (attr = cur_node->properties; attr != NULL; attr = attr->next)
+// //         {
+// //             xmlNode *value = attr->children;
+// //             char *attrName = (char *)attr->name;
+// //             char *cont = (char *)(value->content);
+
+           
+// //             printf("\tattribute name: %s, attribute value = %s\n", attrName, cont);
+// //             // printf("Parent %s\n", (char*)(cur_node->psvi)	);
+// //         }
+
+// //         print_element_names(cur_node->children);
+// //     }
+// // }   
+
+
+// static void
+// print_element_names(xmlNode * a_node)
+// {
+//     xmlNode *cur_node = NULL;
+    
+//     for (cur_node = a_node; cur_node != NULL; cur_node = cur_node->next){
+//         if (cur_node->type == XML_ELEMENT_NODE) {
+//             if(strcmp((char *)(cur_node->name), "g") == 0){
+//                 //xmlAttr *attr;
+//                 printf("node type: Element, name: %s\n", (char *)cur_node->name);
+//                 cur_node = cur_node->children;
+                
+//                  for (cur_node =  cur_node->next; cur_node->next != NULL; cur_node = cur_node->next){
+//                     printf("node type: Element, name: %s\n", (char *)cur_node->name);
+
+//                      xmlAttr *attr;
+//                     for (attr = cur_node->properties; attr != NULL; attr = attr->next)
+//                     {
+//                         xmlNode *value = attr->children;
+//                         char *attrName = (char *)attr->name;
+//                         char *cont = (char *)(value->content);
+
+
+//                         printf("\tattribute name: %s, attribute value = %s\n", attrName, cont);
+//                     }
+
+//                  }
+
+//             }
+//         }
+
+//         print_element_names(cur_node->children);
+//     }
+// }
+
+
+
+// // static void
+// // print_element_names(xmlNode * a_node)
+// // {
+// //     xmlNode *cur_node = NULL;
+
+// //     for (cur_node = a_node; cur_node != NULL; cur_node = cur_node->next) {
+// //         if (cur_node->type == XML_ELEMENT_NODE) {
+    
+// //              if(strcmp((char *)(cur_node->name), "g") == 0){
+// //                 cur_node = cur_node->next;
+// //              }
+            
+// //                 printf("node type: Element, name: %s\n", cur_node->name);            
+
+// //                 xmlAttr *attr;
+// //                 for (attr = cur_node->properties; attr != NULL; attr = attr->next)
+// //                 {
+// //                     xmlNode *value = attr->children;
+// //                     char *attrName = (char *)attr->name;
+// //                     char *cont = (char *)(value->content);
+
+
+// //                     printf("\tattribute name: %s, attribute value = %s\n", attrName, cont);
+// //                     // printf("Parent %s\n", (char*)(cur_node->psvi)	);
+// //                 }
+
+// //         }
+
+// //         print_element_names(cur_node->children);
+// //     }
+// // }   
+
+
+
+
+// /**
+//  * Simple example to parse a file called "file.xml", 
+//  * walk down the DOM, and print the name of the 
+//  * xml elements nodes.
+//  */
+// int
+// main(int argc, char **argv)
+// {
+//     xmlDoc *doc = NULL;
+//     xmlNode *root_element = NULL;
+
+//     if (argc != 2){
+//         printf("usage: xmlExample <someXMLfile>\n");
+
+//         return(1);
+//     }
+
+//     /*
+//      * this initialize the library and check potential ABI mismatches
+//      * between the version it was compiled for and the actual shared
+//      * library used.
+//      */
+//     LIBXML_TEST_VERSION
+
+//     /*parse the file and get the DOM */
+//     doc = xmlReadFile(argv[1], NULL, 0);
+
+//     if (doc == NULL) {
+//         printf("error: could not parse file %s\n", argv[1]);
+//     }
+
+//     /*Get the root element node */
+//     root_element = xmlDocGetRootElement(doc);
+
+//     print_element_names(root_element);
+
+//     /*free the document */
+//     xmlFreeDoc(doc);
+
+//     /*
+//      *Free the global variables that may
+//      *have been allocated by the parser.
+//      */
+//     xmlCleanupParser();
+
+//     return 0;
+// }
